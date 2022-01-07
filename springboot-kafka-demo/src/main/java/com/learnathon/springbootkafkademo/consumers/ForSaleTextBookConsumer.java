2ed70c7f30291d1785462a4d7128ca56b05a4d33
David L. Day <dday376@gmail.com>
2022-01-07 13:49:31 -0500

    use json schema payload

--- springboot-kafka-demo/src/main/java/com/learnathon/springbootkafkademo/consumers/ForSaleTextBookConsumer.java
+++ springboot-kafka-demo/src/main/java/com/learnathon/springbootkafkademo/consumers/ForSaleTextBookConsumer.java
@@ -2,6 +2,8 @@ package com.learnathon.springbootkafkademo.consumers;
 
 import java.io.IOException;
 
+import com.learnathon.springbootkafkademo.entities.NewForSaleTextBookEvent;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,15 @@ public class ForSaleTextBookConsumer {
 
   @KafkaListener(topics = "demo_david", groupId = "demo_david")
   public void receiveForSaleTextBookEvent(
-      ConsumerRecord<String, String> msg
+      ConsumerRecord<String, NewForSaleTextBookEvent> msg
     )
-    throws IOException {
-      System.out.println("Got my message! : " + msg.value());
+      throws IOException {
+      NewForSaleTextBookEvent eventMessage = msg.value();
+      System.out.println(
+        "Got my message! Book Name: " + eventMessage.getBookName()
+      );
+      System.out.println(
+        "Got my message! Book Price: " + eventMessage.getPrice()
+      );
     }
 }
--- springboot-kafka-demo/src/main/java/com/learnathon/springbootkafkademo/services/ForSaleTextBookProducer.java
+++ springboot-kafka-demo/src/main/java/com/learnathon/springbootkafkademo/services/ForSaleTextBookProducer.java
@@ -1,5 +1,7 @@
 package com.learnathon.springbootkafkademo.services;
 
+import com.learnathon.springbootkafkademo.entities.NewForSaleTextBookEvent;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -7,10 +9,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ForSaleTextBookProducer {
   @Autowired
-  private KafkaTemplate<String, String> kafkaTemplate;
+  private KafkaTemplate<String, NewForSaleTextBookEvent> kafkaTemplate;
 
   public void sendMessage(String message) {
-    this.kafkaTemplate.send("demo_david", message, message);
+    NewForSaleTextBookEvent newEvent =
+        new NewForSaleTextBookEvent(message, 25.00);
+    this.kafkaTemplate.send("demo_david", message, newEvent);
   }
 
 }
